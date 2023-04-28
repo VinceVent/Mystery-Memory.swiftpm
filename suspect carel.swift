ecac28296883370cdeb435e720fe812bad2a24f0
Vincent Chen <Vchen4070@stu.d214.org>
2023-04-28 13:00:04 -0500

    sus

---
+++ suspect carel.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+struct SusView2: View {
+    //  @Binding var wordplay: WordPlay
+    var body: some View {
+        
+        ZStack{
+            Rectangle()
+            VStack{
+                Text("Joy - I was baking a cake in the second kitchen.")  
+                Divider()
+                Text("Pugo - I was getting a hotdog for everyone. (4 hotdog in hand) ")  
+                Divider()
+                Text("Garp - I went on a walk to the park ")  
+                Divider()
+                Text("Kat - I was with the cat eating in the kitchen.")  
+                Divider()
+                NavigationLink("who's the Murder?"){
+                    DecideView2()
+                    
+                }
+                
+                .foregroundColor(.yellow)
+                .padding()
+                .font(.title3)
+                .background(.gray)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+            }      
+            .foregroundColor(.white)         
+            //      .font(.title3)  
+            
+            
+            
+        }
+        
+    }
+}
+

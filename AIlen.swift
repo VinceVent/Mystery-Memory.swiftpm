748dda8aa615537bb42591b2ffbfaa15685986e1
Vincent Chen <Vchen4070@stu.d214.org>
2023-05-02 13:10:44 -0500

    allen

---
+++ AIlen.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+struct AllenView: View {
+    //  @Binding var wordplay: WordPlay
+    var body: some View {
+        ZStack{
+            Rectangle()
+            VStack{
+                VStack{
+                    Text("age:17 ")
+                    
+                    Text("Address:Basement  Rd")
+                    
+                    Text("Hate: techology,phones,job, pizza, stuff animal")
+                    
+                    Text("Like: trees, animals, school, pots, glasses ")
+                    Text("Weather: ")
+                    
+                    
+                    Text("Cause of death: electrocuted by a eel ")
+                    Text("Date of Death: ")
+                    Text("Inguires: ")
+                    Text("Body found: ")
+                    Text("Suspect: ")
+                    
+                }
+                
+                .foregroundColor(.white)
+                
+                NavigationLink("Suspect alibi"){
+                    SusView3()
+                }
+                .foregroundColor(.yellow)
+                .padding()
+                .font(.title3)
+                .background(.gray)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+            }
+            .navigationTitle("Jullia")
+            
+        }
+    }
+}
+
+

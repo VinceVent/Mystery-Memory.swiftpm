d4f51d15fd42430347ee0fec94c97e49b2b64dc7
James Balek <jbalek6020@stu.d214.org>
2023-05-02 13:19:14 -0500

    e

--- File.swift
+++ File.swift
@@ -2,9 +2,9 @@ import SwiftUI
 struct DetView: View {
     // @Binding var wordplay: WordPlay
     var body: some View {
-        Text("This game is like any murder mystery solving game but with it’s own unique style.")
-          Text(  " You are the detective that will be solving the case and find out who was the murder out of the suspect, but you can only look over the file for a limited amount of time, which when hit 0 u loose, but you can stay on one file/document for long as you like to think on how to solve the mystery.")
-           Text( " One last thing, you will have a note pad to write down any notes you have and that is all.")
+        Text("This game is like any mystery solving game but with it’s own unique style.")
+          Text("You are the detective that will be solving the case and find out who was the person who did something out of the 4 suspects, but you can only look over the file for a limited amount of time, which when hit 0 u lose, but you can stay on one file/document for long as you like to think on how to solve the mystery.")
+           Text("You will have a notepad to write down any notes you have.")
        
     }
 }

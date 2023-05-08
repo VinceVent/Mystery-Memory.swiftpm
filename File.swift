43684225c6667609f51ca4d2f3c5152f70af6422
Vincent Chen <Vchen4070@stu.d214.org>
2023-05-08 12:26:06 -0500

    introduction page look

--- File.swift
+++ File.swift
@@ -2,7 +2,22 @@ import SwiftUI
 struct DetView: View {
     // @Binding var wordplay: WordPlay
     var body: some View {
-        Text("This game is like any mystery solving game but with it’s own unique style. You are the detective that will be solving the case and find out who was the person who did something out of the 4 suspects, you can stay on one file/document for long as you like to think on how to solve the mystery. You will also have a notepad to write down any notes you have.")
+        ZStack{
+            Rectangle()
+            Rectangle()
+            ZStack{
+                Color(.blue)
+                  .frame(width: 370, height: 350)
+                
+                Text("This game is like any mystery solving game but with it’s own unique style. You are the detective that will be solving the case and find out who was the person who did something out of the 4 suspects, you can stay on one file/document for long as you like to think on how to solve the mystery. You will also have a notepad to write down any notes you have.")
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            }
+            
+        }
+        
+            
+        
        
     }
 }

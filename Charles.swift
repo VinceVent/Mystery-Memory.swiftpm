f01cea2f9d43274bdf3d8d11a353d8fdfc8e849a
Vincent Chen <Vchen4070@stu.d214.org>
2023-05-12 12:44:38 -0500

    charles

--- Charles.swift
+++ Charles.swift
@@ -27,7 +27,8 @@ struct CharlesView: View {
                     Text("Stolen Item: His car")
                     Text("Date of when it was stolen: A Tuesday")
                     Text("Suspects: George, Ron, Michael, Harry")
-                    Text("Charles was just minding his business eating his well done Steak, then suddenly he heard his car running off, The suspect")
+                    Text("Charles was just minding his business eating his well done Steak, then suddenly he heard his car running off. When the car was found, it was at a house with 4 people, who became the suspect of the case.")
+                        .frame(width: 360, height: 160, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                 }
                 
                 .foregroundColor(.white)

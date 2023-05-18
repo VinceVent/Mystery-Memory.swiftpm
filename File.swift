f4c399ac26c875df536895f3096495f86594f6ef
James Balek <jbalek6020@stu.d214.org>
2023-05-18 12:42:47 -0500

    e

--- File.swift
+++ File.swift
@@ -7,11 +7,12 @@ struct DetView: View {
             Rectangle()
             ZStack{
                 Color(.blue)
-                  .frame(width: 370, height: 350)
+                  .frame(width: 450, height: 420)
                 
                 Text("This game is like any mystery solving game but with it’s own unique style. You are the detective that will be solving the case and find out who was the person who did something out of the 4 suspects, you can stay on one file/document for long as you like to think on how to solve the mystery. You will also have a notepad to write down any notes you have.")
                     .foregroundColor(.white)
-                    .frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .frame(width: 400, height: 400, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .font(.system(size: 25))
             }
             
         }

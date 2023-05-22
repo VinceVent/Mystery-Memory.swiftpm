1f88ec38e31f2ccecc164cdcf8a0c2a207b81d73
Vincent Chen <Vchen4070@stu.d214.org>
2023-05-22 13:16:17 -0500

    notepad changes

--- Notepad.swift
+++ Notepad.swift
@@ -5,11 +5,21 @@ import SwiftUI
 struct NotePadView: View {
     @State var NotePad = ""
     var body: some View {
+        
+        Rectangle()
         VStack{
+            
+            
+            
+            Text("\(NotePad)")
+                .frame(width: 400, height: 400, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .bold()
             TextField("Write Notes Here", text: $NotePad)
+                .frame(width: 145, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                 .textFieldStyle(.roundedBorder)
-                
+            
         }
         .padding()
+        Rectangle()
     }
 }

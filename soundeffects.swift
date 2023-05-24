53a1c6d1d959f05ee1ca98b70c1e6d9f92fb22f7
James Balek <jbalek6020@stu.d214.org>
2023-05-24 12:25:07 -0500

    file

---
+++ soundeffects.swift
@@ -0,0 +1,21 @@
+//
+//  soundeffects.swift
+//  My App
+//
+//  Created by James Balek on 5/24/23.
+//
+
+import SwiftUI
+
+
+struct soundeffects: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct soundeffects_Previews: PreviewProvider {
+    static var previews: some View {
+        soundeffects()
+    }
+}

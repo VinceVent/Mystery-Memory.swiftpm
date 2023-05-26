22f893054567ed63d0b07971311ea81b81338f8b
James Balek <jbalek6020@stu.d214.org>
2023-05-26 13:26:19 -0500

    sound effect

--- soundeffects.swift
+++ soundeffects.swift
@@ -1,37 +1,20 @@
 import SwiftUI
-import AVKit
+import AVFoundation
 
-class SoundManager {
-    static let instance = SoundManager()
+var player: AVAudioPlayer!
+
     
-    var player: AVAudioPlayer?
-    
-    enum SoundOption: String {
-        case correct
-        case Incorrect
-    }
-    
-    
-    func playSound(sound: SoundOption) {
+func playSound(key: String) {
+        let url = Bundle.main.url(forResource: key, withExtension: "mp3")
         
-        guard let url = Bundle.main.url(forResource: sound.rawValue, withExtension: ".mp3") else { return }
-        
+        guard url != nil else {
+            return
+        }
         do {
-            player = try AVAudioPlayer(contentsOf: url)
-        } catch let error {
-            print("Error playing soung. \(error.localizedDescription)")
+            player = try AVAudioPlayer(contentsOf: url!)
+            player?.play()
+        } catch {
+            print("error")
         }
     }
-}
 
-struct soundeffects: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct soundeffects_Previews: PreviewProvider {
-    static var previews: some View {
-        soundeffects()
-    }
-}
